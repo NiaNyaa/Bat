955bb37e49bb149152c8f1a9a25ee19dd92410c8
NiaNyaa <109681800+NiaNyaa@users.noreply.github.com>
2023-10-25 17:07:40 +0700

    Update ifelse.bat

--- ifelse.bat
+++ ifelse.bat
@@ -4,12 +4,12 @@ setlocal enabledelayedexpansion
 
 set angka=5
 
-if %angka%==5 (
+if !angka!==5 (
 	set hasil=Variabel angka lima
 ) else (
 	set hasil=Variabel bukan angka lima
 )
 
-echo %hasil%
+echo !hasil!
 
 endlocal
